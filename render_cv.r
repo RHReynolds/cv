96479407274dd64d61fb0e3756014a349076a245
Regina H Reynolds <regina.reynolds.16@ucl.ac.uk>
2025-06-15 17:43:22 +0100

    style: replace pipe with native pipe

--- render_cv.r
+++ render_cv.r
@@ -5,7 +5,6 @@
 library(rmarkdown)
 library(here)
 library(stringr)
-library(magrittr)
 
 #---- Main -----------------------------
 
@@ -14,47 +13,40 @@ library(magrittr)
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
-rmds <- 
+rmds <-
   list.files(
     here::here(),
     pattern = "*.rmd"
   )
 
-for(rmd in rmds){
-  
-  name <- 
-    rmd %>% 
-    stringr::str_remove(
-      "\\..*"
-    )
-  
+for (rmd in rmds) {
+  name <- stringr::str_remove(rmd, "\\..*")
+
   print(
     stringr::str_c("Knitting ", name)
-    )
-  
+  )
+
   # Knit rmd to HTML version
   rmarkdown::render(
     stringr::str_c(name, ".rmd"),
     params = list(pdf_mode = FALSE),
-    output_file = 
+    output_file =
       stringr::str_c("docs/", name, ".html")
   )
-  
+
   # Knit the PDF version to temporary html location
   tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-  
+
   rmarkdown::render(
     stringr::str_c(name, ".rmd"),
     params = list(pdf_mode = TRUE),
     output_file = tmp_html_cv_loc
   )
-  
+
   # Convert to PDF using Pagedown
   pagedown::chrome_print(
     input = tmp_html_cv_loc,
-    output = 
+    output =
       stringr::str_c("docs/rhr_", name, ".pdf")
-    )
-  
-  
+  )
 }
