394b6c5bdf5b872830304a9601ebdae8ee9350c3
Regina H Reynolds <regina.reynolds.16@ucl.ac.uk>
2025-06-22 16:10:27 +0100

    refactor: move functions out of others, improve docs

--- R/cv_printing_functions.r
+++ R/cv_printing_functions.r
@@ -1,57 +1,74 @@
 # This file contains all the code needed to parse and print various sections of your CV
 # from data. Feel free to tweak it as you desire!
 
+# Main CV Constructor ----------------------------------------------------------------
 
-#' Create a CV_Printer object.
+#' Create a CV_Printer object
 #'
-#' @param data_location Path of the spreadsheets holding all your data. This can be
-#'   either a URL to a google sheet with multiple sheets containing the four
-#'   data types or a path to a folder containing an xlsx file with 4 sheets with the
-#'   the necessary info.
-#' @param source_location Where is the code to build your CV hosted?
-#' @param pdf_mode Is the output being rendered into a pdf? Aka do links need
-#'   to be stripped?
-#' @param sheet_is_publicly_readable If you're using google sheets for data,
-#'   is the sheet publicly available? (Makes authorization easier.)
-#' @return A new `CV_Printer` object.
-create_CV_object <- function(data_location,
-                             pdf_mode = FALSE,
-                             sheet_is_publicly_readable = TRUE) {
+#' Constructs a CV_Printer object from either a Google Sheet or a local Excel file.
+#'
+#' @param data_location Path to the data source. Either a URL to a Google Sheet or a directory
+#'   containing a `.xlsx` file with the required sheets.
+#' @param pdf_mode Logical. Should links be stripped for PDF rendering? Defaults to `FALSE`.
+#' @param sheet_is_publicly_readable Logical. If using Google Sheets, is it publicly accessible?
+#'   Defaults to `TRUE`.
+#' @return A list (CV_Printer object) containing CV data and utilities.
+#' @export
+create_CV_object <- function(
+  data_location,
+  pdf_mode = FALSE,
+  sheet_is_publicly_readable = TRUE
+) {
   cv <- list(
     pdf_mode = pdf_mode,
     links = c()
   )
 
-  is_google_sheets_location <- stringr::str_detect(data_location, "docs\\.google\\.com")
+  is_google_sheets_location <- stringr::str_detect(
+    data_location,
+    "docs\\.google\\.com"
+  )
 
   if (is_google_sheets_location) {
     if (sheet_is_publicly_readable) {
-      # This tells google sheets to not try and authenticate. Note that this will only
-      # work if your sheet has sharing set to "anyone with link can view"
       googlesheets4::sheets_deauth()
     } else {
-      # My info is in a public sheet so there's no need to do authentication but if you want
-      # to use a private sheet, then this is the way you need to do it.
-      # designate project-specific cache so we can render Rmd without problems
       options(gargle_oauth_cache = ".secrets")
     }
 
-    read_gsheet <- function(sheet_id) {
-      googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
-    }
-    cv$entries_data <- read_gsheet(sheet_id = "entries")
-    cv$skills <- read_gsheet(sheet_id = "language_skills")
-    cv$text_blocks <- read_gsheet(sheet_id = "text_blocks")
-    cv$contact_info <- read_gsheet(sheet_id = "contact_info")
+    cv$entries_data <- read_gsheet(data_location, "entries")
+    cv$skills <- read_gsheet(data_location, "language_skills")
+    cv$text_blocks <- read_gsheet(data_location, "text_blocks")
+    cv$contact_info <- read_gsheet(data_location, "contact_info")
   } else {
-    xlsx_file <- list.files(path = data_location, pattern = ".xlsx", full.names = TRUE)
-    cv$entries_data <- readxl::read_excel(xlsx_file, sheet = "entries", skip = 1)
-    cv$skills <- readxl::read_excel(xlsx_file, sheet = "language_skills", skip = 1)
-    cv$text_blocks <- readxl::read_excel(xlsx_file, sheet = "text_blocks", skip = 1)
-    cv$contact_info <- readxl::read_excel(xlsx_file, sheet = "contact_info", skip = 1)
+    xlsx_file <- list.files(
+      path = data_location,
+      pattern = ".xlsx",
+      full.names = TRUE
+    )
+    cv$entries_data <- readxl::read_excel(
+      xlsx_file,
+      sheet = "entries",
+      skip = 1
+    )
+    cv$skills <- readxl::read_excel(
+      xlsx_file,
+      sheet = "language_skills",
+      skip = 1
+    )
+    cv$text_blocks <- readxl::read_excel(
+      xlsx_file,
+      sheet = "text_blocks",
+      skip = 1
+    )
+    cv$contact_info <- readxl::read_excel(
+      xlsx_file,
+      sheet = "contact_info",
+      skip = 1
+    )
   }
 
-  # Clean up entries dataframe to format we need it for printing
+  # Clean and enrich entries data
   cv$entries_data <-
     cv$entries_data |>
     tidyr::unite(
@@ -61,7 +78,11 @@ create_CV_object <- function(data_location,
       na.rm = TRUE
     ) |>
     dplyr::mutate(
-      description_bullets = ifelse(description_bullets != "", paste0("- ", description_bullets), ""),
+      description_bullets = ifelse(
+        description_bullets != "",
+        paste0("- ", description_bullets),
+        ""
+      ),
       start = ifelse(start == "NULL", NA, start),
       end = ifelse(end == "NULL", NA, end),
       start_year = .extract_year(start),
@@ -84,46 +105,24 @@ create_CV_object <- function(data_location,
 }
 
 
-# Remove links from a text block and add to internal list
-sanitize_links <- function(cv, text) {
-  if (cv$pdf_mode) {
-    link_titles <- stringr::str_extract_all(text, "(?<=\\[).+?(?=\\])")[[1]]
-    link_destinations <- stringr::str_extract_all(text, "(?<=\\().+?(?=\\))")[[1]]
+# Print Methods ----------------------------------------------------------------------
 
-    n_links <- length(cv$links)
-    n_new_links <- length(link_titles)
-
-    if (n_new_links > 0) {
-      # add links to links array
-      cv$links <- c(cv$links, link_destinations)
-
-      # Build map of link destination to superscript
-      link_superscript_mappings <- purrr::set_names(
-        paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
-        paste0("(", link_destinations, ")")
-      )
-
-      # Replace the link destination and remove square brackets for title
-      text <- text |>
-        stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) |>
-        stringr::str_replace_all("\\[(.+?)\\]", "\\1")
-    }
-  }
-
-  list(cv = cv, text = text)
-}
-
-
-#' @description Take a position data frame and the section id desired and prints the section to markdown.
-#' @param section_id ID of the entries section to be printed as encoded by the `section` column of the `entries` table
-#' @param resume logical vector. Determines whether to filter entries by `in_resume`. Default is FALSE.
-print_section <- function(cv, section_id, glue_template = "default", resume = FALSE) {
-  # TODO: need to re-think this, as filtered cv is saved to object
-  # meaning all following sections will be "resume" format
-  if (resume == TRUE) {
-    cv$entries_data <-
-      cv$entries_data |>
-      dplyr::filter(in_resume == TRUE)
+#' Print a section of entries from the CV
+#'
+#' @param cv CV_Printer object
+#' @param section_id Character. Section ID as found in the `section` column of the entries table.
+#' @param glue_template Optional string template for output formatting. Defaults to a markdown block.
+#' @param resume Logical. If TRUE, filters entries where `in_resume` is TRUE.
+#' @return Invisibly returns the modified CV object.
+#' @export
+print_section <- function(
+  cv,
+  section_id,
+  glue_template = "default",
+  resume = FALSE
+) {
+  if (resume) {
+    cv$entries_data <- dplyr::filter(cv$entries_data, in_resume == TRUE)
   }
 
   if (glue_template == "default") {
@@ -142,8 +141,6 @@ print_section <- function(cv, section_id, glue_template = "default", resume = FA
 
   section_data <- dplyr::filter(cv$entries_data, section == section_id)
 
-  # Take entire entries data frame and removes the links in descending order
-  # so links for the same position are right next to each other in number.
   for (i in 1:nrow(section_data)) {
     for (col in c("title", "description_bullets")) {
       strip_res <- sanitize_links(cv, section_data[i, col])
@@ -153,31 +150,30 @@ print_section <- function(cv, section_id, glue_template = "default", resume = FA
   }
 
   print(glue::glue_data(section_data, glue_template))
-
-  invisible(strip_res$cv)
+  invisible(cv)
 }
 
 
-
-#' @description Prints out text block identified by a given label.
-#' @param label ID of the text block to print as encoded in `label` column of `text_blocks` table.
-print_text_block <- function(cv, label) {
-  text_block <- dplyr::filter(cv$text_blocks, loc == label) |>
-    dplyr::pull(text)
-
-  strip_res <- sanitize_links(cv, text_block)
-
-  cat(strip_res$text)
-
-  invisible(strip_res$cv)
-}
-
-
-
-#' @description Construct a list of skills
+#' Print a text block by label
 #'
-#' @param type_of_interest Character. Filter for skills to print.
+#' @param cv CV_Printer object
+#' @param label Character. Label in the `text_blocks` table to print.
+#' @return Invisibly returns the modified CV object.
+#' @export
+print_text_block <- function(cv, label) {
+  text_block <- dplyr::filter(cv$text_blocks, loc == label) |> dplyr::pull(text)
+  strip_res <- sanitize_links(cv, text_block)
+  cat(strip_res$text)
+  invisible(strip_res$cv)
+}
 
+
+#' Print a list of skills
+#'
+#' @param cv CV_Printer object
+#' @param type_of_interest Character. Type to filter for in the `skills` table.
+#' @return Invisibly returns the CV object.
+#' @export
 print_skill <- function(cv, type_of_interest) {
   cv$skills |>
     dplyr::filter(type == type_of_interest) |>
@@ -185,75 +181,133 @@ print_skill <- function(cv, type_of_interest) {
       "- <i class='fa-{icon_type} fa-{icon}'></i> {skill}"
     ) |>
     print()
-
   invisible(cv)
 }
 
 
-
-#' @description List of all links in document labeled by their superscript integer.
+#' Print all gathered links
+#'
+#' @param cv CV_Printer object
+#' @return Invisibly returns the CV object.
+#' @export
 print_links <- function(cv) {
   n_links <- length(cv$links)
   if (n_links > 0) {
-    cat("
+    cat(
+      "
 Links {data-icon=link}
 --------------------------------------------------------------------------------
 
 <br>
-
-
-")
-
+"
+    )
     purrr::walk2(cv$links, 1:n_links, function(link, index) {
       print(glue::glue("{index}. {link}"))
     })
   }
-
   invisible(cv)
 }
 
 
-
-#' @description Contact information section with icons
+#' Print contact information
+#'
+#' @param cv CV_Printer object
+#' @return Invisibly returns the CV object.
+#' @export
 print_contact_info <- function(cv) {
   glue::glue_data(
     cv$contact_info,
     "- <i class='fa fa-{icon}'></i> {contact}"
-  ) |> print()
-
+  ) |>
+    print()
   invisible(cv)
 }
 
-#' @description Introduce breaks depending on pdf mode
+
+#' Print HTML or PDF breaks
+#'
+#' @param html_breaks Number of breaks for HTML output.
+#' @param pdf_breaks Number of breaks for PDF output.
+#' @param pdf_mode Logical. Whether the output is for PDF.
+#' @export
 print_breaks <- function(html_breaks = 1, pdf_breaks = 1, pdf_mode) {
-  if (pdf_mode) {
-    cat(stringr::str_c(
-      "\n",
-      stringr::str_c(rep("<br>", pdf_breaks), collapse = "\n"),
-      "\n"
-    ))
-  } else {
-    cat(stringr::str_c(
-      "\n",
-      stringr::str_c(rep("<br>", html_breaks), collapse = "\n"),
-      "\n"
-    ))
-  }
+  cat(stringr::str_c(
+    "\n",
+    stringr::str_c(
+      rep("<br>", ifelse(pdf_mode, pdf_breaks, html_breaks)),
+      collapse = "\n"
+    ),
+    "\n"
+  ))
 }
 
-# Helper functions -----------------------------------------------------------------
+# Helpers ---------------------------------------------------------------------------
 
+#' Sanitize markdown links (for PDF output)
+#'
+#' Removes markdown-style links and replaces them with superscript references.
+#'
+#' @param cv CV_Printer object
+#' @param text Character. Input text to sanitize.
+#' @return List with modified CV and sanitized text.
+sanitize_links <- function(cv, text) {
+  if (cv$pdf_mode) {
+    link_titles <- stringr::str_extract_all(text, "(?<=\\[).+?(?=\\])")[[1]]
+    link_destinations <- stringr::str_extract_all(text, "(?<=\\().+?(?=\\))")[[
+      1
+    ]]
+
+    n_links <- length(cv$links)
+    n_new_links <- length(link_titles)
+
+    if (n_new_links > 0) {
+      cv$links <- c(cv$links, link_destinations)
+
+      link_superscript_mappings <- purrr::set_names(
+        paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
+        paste0("(", link_destinations, ")")
+      )
+
+      text <- text |>
+        stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) |>
+        stringr::str_replace_all("\\[(.+?)\\]", "\\1")
+    }
+  }
+  list(cv = cv, text = text)
+}
+
+#' Read Google Sheet by sheet name
+#' @param data_location Google Sheet URL
+#' @param sheet_id Sheet name to read
+#' @return A data.frame of the sheet contents
+read_gsheet <- function(data_location, sheet_id) {
+  googlesheets4::read_sheet(
+    data_location,
+    sheet = sheet_id,
+    skip = 1,
+    col_types = "c"
+  )
+}
+
+#' Extract date from string
+#' @param dates Vector of dates (character)
+#' @return Date vector
 .parse_dates <- function(dates) {
-  date_month <- stringr::str_extract(dates, "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})")
+  date_month <- stringr::str_extract(
+    dates,
+    "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})"
+  )
   date_month[is.na(date_month)] <- "1"
-
   paste("1", date_month, .extract_year(dates), sep = "-") |>
     lubridate::dmy()
 }
 
+#' Extract year from date string
+#' @param dates Vector of date strings
+#' @return Numeric vector of years
 .extract_year <- function(dates) {
   date_year <- stringr::str_extract(dates, "(20|19)[0-9]{2}")
-  date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) + 10
-
+  date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) +
+    10
   date_year
 }
